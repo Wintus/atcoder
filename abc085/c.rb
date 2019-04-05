51fc5145cdded7215563596a9d596382c68de14e
wintus <yuya0726+GitHub@gmail.com>
2019-04-06 05:15:50 +0900

    speed up

--- abc085/c.rb
+++ abc085/c.rb
@@ -1,14 +1,14 @@
-BILLS = [10_000, 5_000, 1_000]
-
 n, y = gets.split.map(&:to_i)
 
 ns = [*0..n]
 combs = ns.product(ns).lazy
-tris = combs.map { |a, b| [a, b, n - a - b] }.reject { |abc| abc.last < 0 }
-tri = tris.find do |cs|
-  total = cs.zip(BILLS).map { |c, v| c * v }.reduce(:+)
+
+tris = combs.map { |a, b| [a, b, n - a - b] }
+r = tris.find do |a, b, c|
+  next if c < 0 # hack
+  total = 10000 * a + 5000 * b + 1000 * c
   total == y
 end
+r ||= [-1] * 3
 
-tri ||= [-1] * 3
-puts tri.join(' ')
+puts r.join(' ')
