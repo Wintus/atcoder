a2ab1450ee67f6a6835609fab27e8e5c6f5eb767
wintus <yuya0726+GitHub@gmail.com>
2019-04-06 00:46:17 +0900

    Refactor solution of ABC 081 B

--- abc081/b.rb
+++ abc081/b.rb
@@ -1,11 +1,5 @@
 _ = gets # .to_i
 ns = gets.split.map(&:to_i)
 
-# anamorphism
-m = 0
-while ns.all?(&:even?)
-  ns.map! { |n| n / 2 }
-  m += 1
-end
-
-p m
+# min LSB
+p ns.map { |n| n & -n }.min.bit_length - 1
