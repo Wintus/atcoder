056351d832c993dd117a0b8f2cbb640dfdbc0100
wintus <yuya0726+GitHub@gmail.com>
2019-04-06 05:23:33 +0900

    speed up more

--- abc085/c.rb
+++ abc085/c.rb
@@ -1,14 +1,16 @@
 n, y = gets.split.map(&:to_i)
 
-ns = [*0..n]
-combs = ns.product(ns).lazy
+t =
+  catch(true) do
+    (0..n).each do |a|
+      (0..(n - a)).each do |b|
+        c = n - a - b
+        next if c < 0
+        total = 10000 * a + 5000 * b + 1000 * c
+        throw(true, [a, b, c]) if total == y
+      end
+    end
+    [-1] * 3
+  end
 
-tris = combs.map { |a, b| [a, b, n - a - b] }
-r = tris.find do |a, b, c|
-  next if c < 0 # hack
-  total = 10000 * a + 5000 * b + 1000 * c
-  total == y
-end
-r ||= [-1] * 3
-
-puts r.join(' ')
+puts t.join(' ')
