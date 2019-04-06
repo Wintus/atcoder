8a5379909f303368d8290a77665f1e7e3e5b7fcc
wintus <yuya0726+GitHub@gmail.com>
2019-04-06 22:36:43 +0900

    Fix ABC123 C

--- abc123/c.rb
+++ abc123/c.rb
@@ -6,31 +6,7 @@ c = gets.to_i
 d = gets.to_i
 e = gets.to_i
 
-cap = [a, b, c, d, e]
+caps = [a, b, c, d, e]
+cap = caps.min # bottle neck
 
-# n of cities
-state = Array.new(6, 0)
-# init
-state[0] = n
-
-# greedy
-cap.reverse!
-state.reverse!
-
-# 1 min 1 step
-# greedy
-def move!(ts, state)
-  ts.each.with_index(1) do |cap, city|
-    n = [state[city], cap].min
-    state[city] -= n
-    state[city - 1] += n
-  end
-end
-
-t = 0
-until state.first == n
-  move!(cap, state)
-  t += 1
-end
-
-p t
+p 5 + n / cap
