3b47ab5fae074dc39a01cd7b84dedea3ca1c601e
wintus <yuya0726+GitHub@gmail.com>
2019-04-13 05:01:11 +0900

    Speedup ABC051 more
    avoid array allocation

--- abc051/b.rb
+++ abc051/b.rb
@@ -1,7 +1,9 @@
 k, s = gets.split.map(&:to_i)
 
 ks = 0..k
-ns = [*ks]
-xy = ns.product(ns) # 3 -> 2
-zs = xy.map { |x, y| s - x - y }
-p zs.count { |z| ks.include?(z) }
+ns = ks.to_a
+
+c = 0
+# 3 -> 2
+ns.product(ns) { |x, y| c += 1 if ks.include?(s - x - y) } # z
+p c
