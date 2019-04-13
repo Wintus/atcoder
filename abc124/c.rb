1c8f73f47b51e2ebb346b9c96cf12cb833bd9cc2
wintus <yuya0726+GitHub@gmail.com>
2019-04-14 02:16:55 +0900

    Refactor C to reduce memory

--- abc124/c.rb
+++ abc124/c.rb
@@ -3,15 +3,14 @@ n = s.size
 
 if n == 1
   p 0
-  exit
+else
+  d, m = n.divmod(2)
+  s0 = '01' * d + '0' * m
+  s1 = '10' * d + '1' * m
+
+  def diff_count(a, b)
+    a.size.times.count { |i| a[i] != b[i] }
+  end
+
+  p [diff_count(s, s0), diff_count(s, s1)].min
 end
-
-d, m = n.divmod(2)
-s0 = '01' * d + '0' * m
-s1 = '10' * d + '1' * m
-
-def diff_count(a, b)
-  a.chars.zip(b.chars).count { |(b0, b1)| b0 != b1 }
-end
-
-p [diff_count(s, s0), diff_count(s, s1)].min
