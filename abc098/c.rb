5b28b14f6c1ee282f2ff032963918471593c8673
wintus <yuya0726+GitHub@gmail.com>
2019-04-27 19:07:48 +0900

    Fix solution of ABC098-C by Ruby

--- abc098/c.rb
+++ abc098/c.rb
@@ -10,4 +10,4 @@ s = gets.chomp.chars
 ws = s.map { |c| c.count 'W' }.cum_sum
 es = s.map { |c| c.count 'E' }.cum_sum
 
-p Array.new(n) { |i| ws[i] + es.last - es[i] }.min
+p Array.new(n) { |i| ws[i] + es.last - es[i + 1] }.min
