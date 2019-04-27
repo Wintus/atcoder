a26980fbc0d7aa0813bed74265cc467b7c56acc4
wintus <yuya0726+GitHub@gmail.com>
2019-04-28 00:07:42 +0900

    Rename enum lib in Ruby

--- lib/enum.rb
+++ lib/enum.rb
@@ -1,9 +1,9 @@
 module Enumerable
-  def scan_left(init)
+  def scan(init)
     inject([init]) { |a, n| a << yield(a.last, n) }
   end
 
   def cum_sum
-    scan_left(0, &:+)
+    scan(0, &:+)
   end
 end
