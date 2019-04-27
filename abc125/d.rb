d476e48d98395edb9430ba66ec65d9371eed760a
wintus <yuya0726+GitHub@gmail.com>
2019-04-27 22:23:55 +0900

    WIP: D is WA

--- abc125/d.rb
+++ abc125/d.rb
@@ -1,19 +1,25 @@
+FLIP2 = [[0, -1], [-1, 0]]
+FLIP3 = [-1, 1, -1]
+
 n = gets.to_i
 ns = gets.split.map(&:to_i)
 
 n.times do
-  ms = ns.each_cons(2).map { |(a, b)| a + b }
-  neg = ms.any?(&:negative?)
-  p ms, neg
-  break if neg
+p  signs = ns.map { |m| m <=> 0 }
+  _t, i = signs.each_cons(2).with_index.find { |pair, _i| FLIP2.include?(pair) }
+  break if i.nil?
 
-  _, i = ms.each_with_index.max_by { |m, _| m * -1 }
-  p i
+  ns[i] *= -1
+  ns[i + 1] *= -1
+end
 
-  ns[i] = ns[i] * -1
-  ns[i + 1] = ns[i + 1] * -1
+n.times do
+ p signs = ns.map { |m| m <=> 0 }
+  _t, i = signs.each_cons(3).with_index.find { |tri, _i| tri == FLIP3 }
+  break if i.nil?
 
-  p ns
+  ns[i] *= -1
+  ns[i + 2] *= -1
 end
 
 p ns.reduce(:+)
