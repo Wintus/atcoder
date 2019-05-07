7a2e218beda63fd6814aa341ef3130681a72ff20
wintus <yuya0726+GitHub@gmail.com>
2019-05-08 02:54:18 +0900

    tweak var name

--- agc023/a.rb
+++ agc023/a.rb
@@ -1,11 +1,11 @@
 gets # n
 ns = gets.split.map(&:to_i)
 
-counts = Hash.new(0)
+counter = Hash.new(0)
 # cumulative sums
 ns.inject([0]) { |a, n| a << a.last + n }.each do |n|
-  counts[n] += 1
+  counter[n] += 1
 end
 
 # sum of nC2
-p counts.each_value.reduce(0) { |a, n| a + n * (n - 1) / 2 }
+p counter.each_value.reduce(0) { |a, n| a + n * (n - 1) / 2 }
