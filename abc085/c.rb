c302090cb850a18441d8255718383e48670fd452
wintus <yuya0726+GitHub@gmail.com>
2019-05-12 06:32:50 +0900

    Revert "refactor by chain" due to memory bloat
    This reverts commit 539241b7

--- abc085/c.rb
+++ abc085/c.rb
@@ -1,11 +1,16 @@
 n, y = gets.split.map(&:to_i)
 
 t =
-  (0..n)
-  .flat_map { |a| (0..(n - a)).map { |b| [a, b] } }
-  .lazy
-  .map { |(a, b)| [a, b, n - a - b] }
-  .find { |(a, b, c)| 10_000 * a + 5000 * b + 1000 * c == y }
-t ||= [-1] * 3
+  catch(true) do
+    (0..n).each do |a|
+      (0..(n - a)).each do |b|
+        c = n - a - b
+        next if c < 0
+        total = 10000 * a + 5000 * b + 1000 * c
+        throw(true, [a, b, c]) if total == y
+      end
+    end
+    [-1] * 3
+  end
 
 puts t.join(' ')
