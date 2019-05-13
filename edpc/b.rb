3ae7ff91c5c099efdd26cda1514d89ee4e130160
wintus <yuya0726+GitHub@gmail.com>
2019-05-13 23:56:21 +0900

    rewrite by ahead refer (TLE)

--- edpc/b.rb
+++ edpc/b.rb
@@ -3,14 +3,13 @@ INF = 1 << 60
 n, k = gets.split.map(&:to_i)
 hs = gets.split.map(&:to_i)
 
-# minimize
-# init
-dp = Array.new(n, INF)
-dp[0] = 0
-# change min
+dp = [0]
+# refer ahead
 n.times do |i|
   (i + 1).upto([i + k, n - 1].min) do |j|
-    dp[j] = [dp[j], dp[i] + (hs[i] - hs[j]).abs].min
+    # inline change min
+    cost = dp[i] + (hs[i] - hs[j]).abs
+    dp[j] = cost if dp.fetch(j, INF) > cost
   end
 end
 
