eb20fc57355333143da29867fbdc1818235f50dd
wintus <yuya0726+GitHub@gmail.com>
2019-05-19 23:27:17 +0900

    Resolve D

--- abc126/d.rb
+++ abc126/d.rb
@@ -1,22 +1,29 @@
-tree = Hash.new { |h, k| h[k] = {} }
+GRAPH = Hash.new { |h, k| h[k] = {} }
+
 n = gets.to_i
 (n - 1).times do
   u, v, w = gets.split.map(&:to_i)
-  tree[u - 1][v - 1] = w # 0-origin
+  # 0-origin
+  u -= 1
+  v -= 1
+  GRAPH[u][v] = w
+  GRAPH[v][u] = w
 end
 
-# colors <0, 1>
-cs = Array.new(n, 0)
+# <0, 1>
+COLORS = Array.new(n, 0)
 
-# @param [Hash] tree
-# @param [Array] cs
-# @param [Integer] i starting
-# @param [Integer] p parity
-def dfs(tree, cs, i = 0, p = 1)
-  cs[i] = p
-  tree[i].each do |j, d|
-    dfs(tree, cs, j, p + d % 2)
+# DFS the graph as a tree
+# @param [Integer] v vertex
+# @param [Integer, nil] p parent
+# @param [Integer] color 0, 1
+def dfs(v = 0, p = nil, color = 1)
+  COLORS[v] = color
+  GRAPH[v].each do |next_v, d|
+    dfs(next_v, v, color ^ (d & 1)) unless next_v == p
   end
 end
 
-p *cs
+dfs
+
+p(*COLORS)
