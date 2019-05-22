343276df9032a1e9b49b1bdc62a3a9d7a08ccdc9
wintus <yuya0726+GitHub@gmail.com>
2019-05-22 19:39:30 +0900

    Resolve F

--- abc126/f.rb
+++ abc126/f.rb
@@ -1,11 +1,24 @@
 m, k = gets.split.map(&:to_i)
 
-if k >= 2 ** m || (k == 1 && m == 1)
-  p -1
-else
-  ns = []
-  (2 ** m).times do |i|
-    ns << i << (i ^ k)
+if m == 0
+  if k != 0
+    p -1
+  else
+    puts '0 0'
   end
-  puts ns.join ' '
+elsif m == 1
+  if k != 0
+    p -1
+  else
+    puts '0 0 1 1'
+  end
+elsif k >= 2 ** m
+  p -1
+elsif k == 0
+  max = 1 << m
+  puts [*0..max, *max.downto(0)].join(' ')
+else
+  max = 1 << m
+  ns = [*0..k, *(k+1)..max]
+  puts [*ns, k, *ns.reverse, k].join(' ')
 end
